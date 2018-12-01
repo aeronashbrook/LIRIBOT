122acc1880b4e87ad4dec983925c090d6f7d572a
Aeron Ashbrook <aeronashbrook@stp-er-10-129-255-3.wireless.umn.edu>
2018-12-01 13:01:49 -0600

    pretty much completed

--- liri.js
+++ liri.js
@@ -1,15 +1,15 @@
 require("dotenv").config();
 var keys = require("./keys.js");
-var spotify = new Spotify(keys.spotify);
 var Spotify = require("node-spotify-api");
 var axios = require("axios");
+var spotify = new Spotify(keys.spotify);
 var input = process.argv;
 var operation = input[2];
-var searchItem = input[3];
+var searchItem = "";
 
 if (operation === "concert-this") {
+    searchItem = input[3]
     var concertUrl = "https://rest.bandsintown.com/artists/" + searchItem + "/events?app_id=codingbootcamp";
-    searchItem = searchItem.replace(" ", "%20");
     axios.get(concertUrl).then(
         function(response) {
             console.log("-----------------")
@@ -20,20 +20,13 @@ if (operation === "concert-this") {
         }
       );  
 } else if (operation === "spotify-this-song") {
-    spotify.search({ type: 'track', query: searchItem }, function (err, data) {
-        if (err) {
-            return console.log('Error occurred: ' + err);
-        }
-        console.log("-----------------")
-        console.log(searchItem);
-        console.log(data.tracks.items[0].album.name);
-        console.log(data.tracks.items[0].preview_url);
-        console.log(JSON.stringify(data.tracks.items[0].artists[0].name, null, 2));
-        console.log("-----------------")
-    });
+  song();
 } else if (operation === "movie-this") {
-    var movieUrl = "http://www.omdbapi.com/?t=" + searchItem + "&y=&plot=short&apikey=trilogy";
+    for (var i = 3;  i < input.length; i++){
+        searchItem = searchItem + " " + input[i];
+    };
     searchItem = searchItem.replace(" ", "%20");
+    var movieUrl = "http://www.omdbapi.com/?t=" + searchItem + "&y=&plot=short&apikey=trilogy";
     axios.get(movieUrl).then(function (response) {
         console.log("-----------------")
         console.log(response.data.Title);
@@ -50,10 +43,33 @@ if (operation === "concert-this") {
     var fs = require("fs");
     fs.readFile("random.txt", "utf8", function (err, data) {
         if (err) {
-            return console.log(err);
+        console.log(err);
         }
-        console.log(data);
+        data = data.split('"');
+        var dataArr = data;
+        operation = dataArr[0].split(',')[0];
+        searchItem = dataArr[1];
+        song();
     });
 } else {
     console.log("Please enter a valid operation")
 }
+
+
+function song(){
+    for (var i = 3;  i < input.length; i++){
+        searchItem = searchItem + " " + input[i];
+    };
+    searchItem = searchItem.replace(" ", "%20");
+    spotify.search({ type: 'track', query: searchItem }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+        console.log("-----------------")
+        console.log(searchItem);
+        console.log(data.tracks.items[0].album.name);
+        console.log(data.tracks.items[0].preview_url);
+        console.log(JSON.stringify(data.tracks.items[0].artists[0].name, null, 2));
+        console.log("-----------------")
+});
+};
